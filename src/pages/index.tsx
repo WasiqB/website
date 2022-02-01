a8986e2025b31ad3167fa40ccdbcfa599d275ce7
WasiqB <wasbhamla2005@gmail.com>
2022-02-01 22:07:34 +0530

    fix: Moved data objects from pages to contents folder.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,32 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Features from '../components/Features';
-import ServiceList from '../contents/services.json';
-import { ButtonType } from '../components/ActionButton';
-import { GitHubButtonType } from '../components/GitHubCountButton';
+import { Features } from '../components/Features';
 import { PageHero } from '../components/PageHero';
+import Data from '../contents/home.json';
 
-const gitButtons: GitHubButtonType[] = [
-  {
-    id: 1,
-    userId: 'WasiqB',
-    type: 'Follow',
-    repoName: undefined,
-  },
-];
-
-const buttons: ButtonType[] = [
-  {
-    id: 1,
-    type: 'button--primary',
-    target: '_self',
-    text: 'About',
-    href: '/about',
-  },
-];
-
-function HomepageHeader({ title, description }): JSX.Element {
+const HomepageHeader = ({ title, description }): JSX.Element => {
   return (
     <PageHero
       image={'/img/home_banner.jpg'}
       tagLine={description}
       title={title}
-      buttons={buttons}
-      gitButtons={gitButtons}
+      buttons={Data.buttons}
+      gitButtons={Data.gitButtons}
     />
   );
-}
+};
 
-export default function Home(): JSX.Element {
+const Home = (): JSX.Element => {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout title={siteConfig.title} description={siteConfig.tagline}>
       <HomepageHeader title={siteConfig.title} description={siteConfig.tagline} />
       <main>
-        <Features features={ServiceList} />
+        <Features features={Data.services} />
       </main>
     </Layout>
   );
-}
+};
+
+export default Home;
